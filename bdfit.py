fixing units issues

bdfit.py
@@ -49,7 +49,7 @@ class BDSampler(object):
         self.name = obj_name
 
         self.model = ModelGrid(spectrum,model,params)
-        print spectrum.keys()
+        #print spectrum.keys()
 
         self.ndim = len(params)
 
@@ -67,7 +67,7 @@ class BDSampler(object):
             then flattens the chain
         """
 
-        nwalkers, nsteps = self.ndim*100, self.ndim*500
+        nwalkers, nsteps = self.ndim*4, self.ndim*10
         p0 = np.zeros((nwalkers,self.ndim))
         for i in range(nwalkers):
              p0[i] = self.start_p + (1e-2*np.random.randn(self.ndim)*
@@ -81,7 +81,7 @@ class BDSampler(object):
 #                 self.model.plims[self.model.params[i]]['max'],
 #                 size=nwalkers)
 
-        print p0.shape, nsteps/10
+        #print p0.shape, nsteps/10
         sampler = emcee.EnsembleSampler(nwalkers,self.ndim,self.model)
         pos, prob, state = sampler.run_mcmc(p0,nsteps/10)
         sampler.reset()

calc_chisq.py
@@ -5,11 +5,12 @@
 
 
 import numpy as np
+from astropy import units as u
+import matplotlib.pyplot as plt
 
 # config loads database and makes it available as db
 from config import * 
 from get_mod import *
-import matplotlib.pyplot as plt
 
 def calc_chisq(data_flux,data_unc,model_flux):
     return np.sum((data_flux-model_flux)**2/(data_unc**2))
@@ -23,8 +24,9 @@ def test_all(data_wave, data_flux, data_unc, model_dict, params):
     chisq = np.ones(num_models)*(99e15)
 
     for i in range(num_models):
-        mod_flux = falt2(model_dict['wsyn'],model_dict['fsyn'][i],100)
-        mod_flux = np.interp(data_wave*10000,model_dict['wsyn'],mod_flux)
+        #print i, num_models, model_dict['logg'][i], model_dict['teff'][i]
+        mod_flux = falt2(model_dict['wsyn'],model_dict['fsyn'][i],100*u.AA)
+        mod_flux = np.interp(data_wave,model_dict['wsyn'],mod_flux)
         mult1 = data_flux*mod_flux
         bad = np.isnan(mult1)
         mult = np.sum(mult1[~bad])

make_model.py
@@ -6,11 +6,11 @@
 
 import numpy as np
 from astropy import units as u
+import matplotlib.pyplot as plt
 
 # config loads database and makes it available as db
 from config import * 
 from get_mod import *
-import matplotlib.pyplot as plt
 
 class ModelGrid(object):
 
@@ -186,7 +186,7 @@ class ModelGrid(object):
 #                print old_spectra.keys()
             else:
                 print 'WTF'
-        mod_flux = old_spectra[()]
+        mod_flux = old_spectra[()][0]
 #        print 'all done!', len(mod_flux), len(self.flux)
 
         #### NEED A FUNCTION TO DO THE RESAMPLING
@@ -195,7 +195,8 @@ class ModelGrid(object):
         # interpolation resampling
 
         # THIS IS WHERE THE CODE TAKES A LONG TIME
-        mod_flux = falt2(self.model['wsyn'],mod_flux,100)
+        #print p, len(mod_flux)
+        mod_flux = falt2(self.model['wsyn'],mod_flux,100*u.AA)
         mod_flux = np.interp(self.wave*10000,self.model['wsyn'],mod_flux)
 
         # Need to normalize (taking below directly from old makemodel code)

scripts/plot_12_hilow.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 import bdmcmc.sample.fetch as fetch
 
 
@@ -13,37 +14,98 @@ def plot_low(ax,bd,offset,label,pcolor='k'):
     unc = bd.specs['low']['unc']
 
     if len(wav)>1:
+        #print min(wav), max(wav), flu[abs(wav-1.27)<0.005]
         norm_by = np.average(flu[abs(wav-1.27)<0.005])
         flu = flu/norm_by
         unc = flu/norm_by
 
         ax.step(wav,flu+offset,color=pcolor)
-        ax.text(2.2,0.6+offset,label,color=pcolor)
+        ax.text(2.,0.9+offset,label,color=pcolor)
         ax.set_xlim(0.5,3)
+
+        #print 'done', label, np.average(flu[abs(wav-1.27)<0.005])
     else:
         print 'failed', label
 
 
+def plot_high(ax,bd,order,offset,label,pcolor='k'):
 
-#ldwarfs = fetch.fetch_12()
-#bds = ldwarfs.brown_dwarfs
+    wav = bd.specs[order]['wavelength']
+    flu = bd.specs[order]['flux']
+    unc = bd.specs[order]['unc']
+
+    if len(wav)>1:
+        norm_by = np.average(flu)
+        #print label, order, offset, min(wav), max(wav), norm_by
+        flu = flu/norm_by
+        unc = flu/norm_by
+
+        ax.step(wav,flu+offset,color=pcolor)
+        ax.text(plt.gca().get_xlim()[0],1.35+offset,label,color=pcolor)
+        #ax.set_xlim(0.5,3)
+
+        #print 'done', label, np.average(flu[abs(wav-1.27)<0.005])
+    else:
+        print 'failed', label, order
+
+
+ldwarfs = fetch.fetch_12()
+bds = ldwarfs.brown_dwarfs
 unums = bds.keys()
+num_spts = np.zeros(12)
+for i in range(12):
+    num_spts[i] = float(bds[unums[i]].spt[1:])
+unums = np.asarray(unums)[np.argsort(num_spts)]
 
+"""
 offset_jump = 1
 offset_start = 3*offset_jump
 offset = offset_start
 plt.figure(figsize=(12,9))
 ax = plt.subplot(131)
 ax.set_ylim(-0.1,offset_start+1.2)
+ax.set_ylable('flux (normalized at 1.27 microns)')
 for u in unums:
-    plot_low(ax,bds[u],offset,u)
+    plot_low(ax,bds[u],offset,'{} {}'.format(bds[u].spt,u))
+    print u
     if u==unums[3]:
         ax = plt.subplot(132)
         ax.set_ylim(-0.1,offset_start+1.2)
         offset = offset_start
+        ax.set_xlabel('wavelength (micron)')
     elif u==unums[7]:
         ax = plt.subplot(133)
         ax.set_ylim(-0.1,offset_start+1.2)
         offset = offset_start
     else:
         offset -= offset_jump
+plt.savefig('ldwarfs_sample12_{}.png'.format(datetime.date.isoformat(datetime.date.today())))
+"""
+
+
+
+offset_jump = 1.25
+offset_start = 5*offset_jump
+offset = offset_start
+for order in np.asarray([58,59,61,62,63,64,65]):
+    offset = offset_start
+    fig = plt.figure(figsize=(12,9))
+    fig.suptitle('Order {}'.format(order),fontsize='x-large')
+    ax = plt.subplot(121)
+    ax.tick_params(labelsize='large')
+    ax.set_ylim(-0.2,offset_start+1.5)
+    ax.set_ylabel('flux (normalized)',fontsize='x-large')
+    ax.set_xlabel('wavelength (micron)',fontsize='x-large')
+    for u in unums:
+        plot_high(ax,bds[u],order,offset,'{} {}'.format(bds[u].spt,u))
+        #print order, u
+        if u==unums[5]:
+            ax = plt.subplot(122)
+            ax.tick_params(labelsize='large')
+            ax.set_ylim(-0.3,offset_start+1.5)
+            ax.set_ylabel('flux (normalized at 1.27 microns)',
+                 fontsize='x-large')
+            offset = offset_start
+            ax.set_xlabel('wavelength (micron)')
+        else:
+            offset -= offset_jump