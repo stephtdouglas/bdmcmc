ae347a45ac87e8c31c9c48768fd116097c4a667d
stephtdouglas <stephanietdouglas@gmail.com>
2014-02-17 13:12:22 -0500

    pickling high-res dusty models with no smoothing/trimming

---
+++ scripts/pickle_dusty.py
@@ -0,0 +1,36 @@
+# Save the Barman-Phoenix-DUSTY model set to a pickle file without 
+# smoothing/trimming
+# Stephanie Douglas, 17 February 2014
+################################################################################
+
+import logging
+from datetime import date
+
+import bdmcmc.bdfit, bdmcmc.spectra, bdmcmc.get_mod, bdmcmc.make_model, bdmcmc.sample
+from bdmcmc.get_mod import falt2
+import numpy as np
+import astropy.units as u
+from scipy.io.idl import readsav
+
+logging.basicConfig(level=logging.DEBUG)
+
+modelpath = '/vega/astro/users/sd2706/modelSpectra/'
+#modelpath = '/home/stephanie/ldwarfs/summerAMNH/modelSpectra/'
+dustylowfile = modelpath+'modelspeclowresldwarfs.save'
+dl = readsav(dustylowfile)
+d = len(dl.modelspec.teff)
+models = {'teff':np.array([]),'logg':np.array([]),'wsyn':np.array([]),
+     'fsyn':[]}
+
+logging.debug('starting now')
+for i in np.arange(d):
+    models['teff'] = np.append(models['teff'], dl.modelspec.teff[i])
+    models['logg'] = np.append(models['logg'], dl.modelspec.logg[i])
+    models['fsyn'].append(dl.modelspec.fsyn[i]*u.dimensionless_unscaled)
+models['wsyn'] = np.asarray(dl.wsyn)/10000.*u.um
+logging.debug('models retrieved')
+
+
+output = open(modelpath+'dusty_highres.pkl','wb')
+cPickle.dump(modband, output)
+output.close()
