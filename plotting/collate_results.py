a4122285a05a6b036458afea2554f8f94396a2f3
stephtdouglas <stephanietdouglas@gmail.com>
2014-05-30 16:36:39 -0400

    make sure ln(s) gets plotted too

--- plotting/collate_results.py
+++ plotting/collate_results.py
@@ -1,12 +1,13 @@
 
 import logging
 
+import matplotlib.pyplot as plt
 import numpy as np
 import bdmcmc.plotting.compare_results as cr
 import asciitable as at
 import cPickle
 
-result_file = '/home/stephanie/ldwarfs/batch_ldwarfs/Marley_2014_04_28/results_2014-04-28.dat'
+result_file = '/home/stephanie/ldwarfs/batch_ldwarfs/Marley_2014-05-13/results_2014-05-13.dat'
 
 dat = at.read(result_file)
 spts,filename = dat['spt'],dat['filename']
@@ -22,9 +23,9 @@ for i in range(dlen):
     meds.append(med_temp)
     errs.append(err_temp)
 
-all_med = [[[meds[k][i][j] for k in range(dlen)] for j in range(4)] for i in range(3)]
-all_err = [[np.array([errs[k][i][j] for k in range(dlen)]).reshape((2,-1)) for j in range(4)] for i in range(3)]
+all_med = [[[meds[k][i][j] for k in range(dlen)] for j in range(4)] for i in range(4)]
+all_err = [[np.array([errs[k][i][j] for k in range(dlen)]).reshape((2,-1)) for j in range(4)] for i in range(4)]
 
-cr.corner(all_med,all_err,spts,['logg','fsed','teff','ln(s)'],['J','H','K','full'])
+cr.corner(all_med,all_err,spts,['logg','fsed','teff','ln(s)'],['H','K','J','full'])
 
 plt.savefig('compare10_Marley.pdf')
