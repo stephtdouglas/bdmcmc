594f53dcd4539b1692fe940ec497328aadec37c4
stephtdouglas <stephanietdouglas@gmail.com>
2014-06-24 17:56:47 -0400

    smooth for Spex Cross-Dispersed

--- scripts/smooth_r2000.py
+++ scripts/smooth_r2000.py
@@ -17,18 +17,20 @@ logging.basicConfig(level=logging.INFO)
 modelpath = '/home/stephanie/ldwarfs/modelSpectra/'
 am = bdmcmc.get_mod.AtmoModel(modelpath+'marley_ldwarfs.pkl')#,wave_unit=u.um)
 
-summer_name = '2m0345'
 
-medpath =  '/home/stephanie/ldwarfs/summerAMNH/LdwarfSpectra/'
-medpath =  '/vega/astro/users/sd2706/spectra/'
-wm,fm,snrm = rs.medget(rs.med_index(summer_name,'n3'))
+bd = bdmcmc.spectra.BrownDwarf('2057-0252')
+
+spectrum = bdmcmc.spectra.spectrum_query(bd.sid,7,6,obs_date="2006-11-18")
+new_flux_unit = u.erg / (u.um * u.cm**2 * u.s)
+spectrum['flux'] = spectrum['flux'].value*1e4*new_flux_unit
+spectrum['unc'] = spectrum['unc'].value*1e4*new_flux_unit
 
 R = 2000.0 #lambda/delta-lambda
-res = 1.25*u.um/R
+res = 1.5*u.um/R
 
-new_grid = bdmcmc.smooth.smooth_grid(am.model,wm[wm<1.39]*u.um,variable=False,
-    res=res,incremental_outfile='marley_n3_backup.pkl')
+new_grid = bdmcmc.smooth.smooth_grid(am.model,spectrum['wavelength'],
+    variable=False,res=res,incremental_outfile='marley_n3_backup.pkl')
 
-outfile = open(modelpath+'N3_Marley.pkl','wb')
+outfile = open(modelpath+'SXD_Marley.pkl','wb')
 cPickle.dump(new_grid,outfile)
 outfile.close()
