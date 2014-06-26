b3724ccf54d757bcbebf100be24c003d12ad63d6
stephtdouglas <stephanietdouglas@gmail.com>
2014-06-26 17:00:12 -0400

    for loop was set up wrong

--- sample/select_sxd.py
+++ sample/select_sxd.py
@@ -17,7 +17,7 @@ def fetch_sxd():
         " so.shortname, s.source_id FROM spectra AS s JOIN sources AS so ON "
         "so.id=s.source_id WHERE s.telescope_id=7").fetchall()
 
-    # only object names and observation_dateskeep those with resolution 
+    # only keep object names and observation_dates for those with resolution 
     # over 1000 (Prism has R~200-400, XD has R~2000)
     spex_sxd = [[i['shortname'],i['obs_date']] for i in all_spex if 
         (np.average(i['wavelength'][:-1] / np.diff(i['wavelength'])))>1000]
@@ -28,11 +28,9 @@ def make_sxd_batch(model_name="marley",model_file="SXD_marley.pkl"):
 
     spex_sxd = fetch_sxd()
 
+    h = open('submit_all_{}.sh'.format(model_name),'w')
     for name,date in spex_sxd:
-        
-    h = open('submit_all_marley.sh','w')
-    for unum in unums:
-        h.write('qsub run_marley_{}.sh\n'.format(unum))
+        h.write('qsub run_{}_{}.sh\n'.format(model_name,name))
 
         f = open('run_{}_{}.py'.format(model_name,name),'w')
         f.write('import logging\n')
@@ -41,7 +39,7 @@ def make_sxd_batch(model_name="marley",model_file="SXD_marley.pkl"):
         f.write('logging.basicConfig(level=logging.INFO)\n\n')
         f.write("ob = OneBatch('{}',"
              "'/vega/astro/users/sd2706/modelSpectra/{}',"
-             "obs_date={})\n".format(name,model_file,date))
+             "obs_date='{}')\n".format(name,model_file,date))
         f.close()
 
         g = open('run_{}_{}.sh'.format(model_name,name),'w')
@@ -54,12 +52,12 @@ def make_sxd_batch(model_name="marley",model_file="SXD_marley.pkl"):
         g.write("#PBS -m a\n")
         g.write("#PBS -V\n\n")
         g.write("# Set output and error directories\n")
-        g.write("#PBS -o localhost:/vega/astro/users/sd2706/testing/ \n")
-        g.write("#PBS -e localhost:/vega/astro/users/sd2706/testing/ \n")
+        g.write("#PBS -o localhost:/vega/astro/users/sd2706/outputs/ \n")
+        g.write("#PBS -e localhost:/vega/astro/users/sd2706/outputs/ \n")
         g.write("/vega/astro/users/amp2217/yt-x86_64/bin/python"
              " run_{}_{}.py\n".format(model_name,name))
         g.write("# End of script\n")
         g.close()
     h.close()
 
-
+make_sxd_batch()
