d5de0b42a550dded46f4d724dd43c7ff4dec67b5
stephtdouglas <stephanietdouglas@gmail.com>
2014-09-17 20:43:03 -0400

    need to use a different copy of the database

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ base_path = '/vega/astro/users/sd2706/'
 if os.path.exists(base_path)==False:
     base_path = '/home/stephanie/Dropbox/'
 
-db = BDdb.get_db(base_path+'BDNYCdb/BDNYC.db')
+#db = BDdb.get_db(base_path+'BDNYCdb/BDNYC.db')
 
+db = BDdb.get_db(base_path+"paperBD/BDNYC_Yeti.db")
